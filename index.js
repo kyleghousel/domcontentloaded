4d9b56fce42934793bb085efc2546eab616e1ee5
Kyle Housel <kylegranthousel@gmail.com>
2025-05-15 10:10:19 -0400

    Add DOMContentLoaded event listener

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("The DOM has loaded");
+  const p = document.querySelector('#text')
+  p.textContent = 'This is really cool!'
+});
